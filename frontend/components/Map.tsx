84b5090a17aa38b7bd4a33bf4c4e583cc7521639
diki <diki@vasttravelindonesia.com>
2024-10-19 04:45:18 +0000

    new project from idx

---
+++ frontend/components/Map.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Map() {
+  return (
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126748.60965064788!2d107.56075490416707!3d-6.903271951728813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e6398252477f%3A0x146a1f93d3e815b2!2sBandung%2C%20Kota%20Bandung%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1728886311599!5m2!1sid!2sid" width="100%" height="450"></iframe>
+  );
+}
